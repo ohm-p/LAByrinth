fc46fb207b69e61d38aec21d71f7525a2c73af65
ohm-p <parikho@mpfi.org>
2023-02-27 18:33:16 -0500

    new commit

--- gui_video_example.py
+++ gui_video_example.py
@@ -13,71 +13,122 @@ from PyQt6.QtCore import *
 from qt_material import apply_stylesheet
 
 
-class Gui_updater(QThread):
-    update_image = pyqtSignal(np.ndarray)
+
+def spacer():
+# class preview(QObject):
+#     preview_img = pyqtSignal(np.ndarray)
+
+#     def __init__(self):
+#         super().__init__()
+
+#     def run(self):
+#         vid = cv2.VideoCapture(0)
+#         retrieved, frame = vid.read()
+#         if retrieved:
+#             self.update_image.emit(frame)
+#         vid.release()
+    
+
+# class livestream(QObject):
+#     live_img = pyqtSignal(np.ndarray)
 
 
+#     def __init__(self):
+#         super().__init__()
+#         path = "C:\\Users\\ohmkp\\OneDrive\\Desktop\\vids\\"
+#         self.dt = datetime.strftime(datetime.now(), "d%y.%m.%d_t%H.%M")
+#         fourcc = cv2.VideoWriter_fourcc(*'MP4V');fps = 30;frameSize = (1290, 720)
+#         # self.out = cv2.VideoWriter()
+#         self.vid_path = path +  self.dt + "_recording.mp4"
+#         self._gui_run_flag = False
+
+#     def run(self):
+#         self._gui_run_flag = True
+#         vid = cv2.VideoCapture(0)
+#         while self._gui_run_flag:
+#             retrieved, frame = vid.read()
+#             if retrieved:
+#                 self.update_image.emit(frame)
+#             #     rgb_frame = np.column_stack((frame, frame, frame)) 
+#             # imageio.imwrite(self.vid_path, rgb_frame, format = 'FFMPEG')
+#         vid.release()
+
+#     def stop(self):
+#         self._gui_run_flag=False
+#         self.wait()
+
+    pass
+
+
+class processor(QObject):
+    frm = pyqtSignal(np.ndarray)
+    
     def __init__(self):
         super().__init__()
+        self._run_flag = False
         path = "C:\\Users\\ohmkp\\OneDrive\\Desktop\\vids\\"
         self.dt = datetime.strftime(datetime.now(), "d%y.%m.%d_t%H.%M")
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V');fps = 30;frameSize = (1290, 720)
-        self.vid_path = path +  self.dt + "_recording.mp4"
-        self._gui_run_flag = False
+        fourcc = cv2.VideoWriter_fourcc(*'XVID');fps = 30.0;frameSize = (1290, 720)
+        vid_path = path +  self.dt + "_recording.avi"
+        self.out = cv2.VideoWriter(vid_path, fourcc, fps, frameSize)
+        self.vid = cv2.VideoCapture(0);self.vid.release()
+
+
+    def grab_single(self):
+        if not self.vid.isOpened():
+            self.vid.open(0)
+        ret, frame = self.vid.read()
+        if ret:
+            self.frm.emit(frame)
+        else:
+            sys.exit('the camera failed to capture a frame')
+        self.vid.release()
         
-
-    def grab_single_image(self):
-        vid = cv2.VideoCapture(0)
-        retrieved, frame = vid.read()
-        if retrieved:
-            self.update_image.emit(frame)
-            vid.release()
-
-    def run(self):
-        self._gui_run_flag = True
-        vid = cv2.VideoCapture(0)
-        while self._gui_run_flag:
-            retrieved, frame = vid.read()
-            if retrieved:
-                self.update_image.emit(frame)
-                rgb_frame = np.column_stack((frame, frame, frame)) 
-            imageio.imwrite(self.vid_path, rgb_frame, format = 'FFMPEG')
-        vid.release()
-    def stop(self):
-        self._gui_run_flag=False
-        self.wait()
+    def grab_stream(self):
+        self._run_flag = True
+        if not self.vid.isOpened():
+            self.vid.open(0)
+        while self._run_flag:
+            ret, frame = self.vid.read()
+            if ret:
+                self.frm.emit(frame)
+                self.out.write(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                sys.exit('the camera failed to capture a frame')
+        self.vid.release()
+        self.out.release()
 
 
 
-class Maze_Controller(QWidget):
+class Maze_Controller(QWidget, QObject):
     def __init__(self):
         super().__init__()
         #check for settings file
-        if not os.path.exists('./settings.json'):
-            print('Missing settings file ... Loading defaults')
-            #write file for default settings
-            # Rotational values
-            # 4-100 rounds/minute type 0x01
-            # clockwise or counterclockwise 0x00 or 0x01 type 0x01
-            # Stop or Start 0x00 or 0x01 type 0x02
+        # if not os.path.exists('./settings.json'):
+        #     print('Missing settings file ... Loading defaults')
+        #     #write file for default settings
+        #     # Rotational values
+        #     # 4-100 rounds/minute type 0x01
+        #     # clockwise or counterclockwise 0x00 or 0x01 type 0x01
+        #     # Stop or Start 0x00 or 0x01 type 0x02
 
-            #Shocking values
-            #.1-4 *10 mA Type 0x03
-            #stop and start, 0x00 or 0x01 Type 0x04
+        #     #Shocking values
+        #     #.1-4 *10 mA Type 0x03
+        #     #stop and start, 0x00 or 0x01 Type 0x04
 
-            #0xaa, 0xbb, Type(0x01-0x04), Value(varies), Check sum (d2+d3+d4)&0xff, 0xcc, 0xdd
+        #     #0xaa, 0xbb, Type(0x01-0x04), Value(varies), Check sum (d2+d3+d4)&0xff, 0xcc, 0xdd
 
-                #add a way to store shapes in json file
-            # }
-            with open('default_settings.json', 'r') as default_settings:
-                self.settings = json.load(default_settings)
-            #loads the settings from the default file and saves them to a new settings file since there is none
-            with open('settings.json', 'w') as outfile:
-                json.dump(self.settings, outfile)
-        else:
-            print('found settings file')
-            with open('settings.json', 'r') as json_settings:
-                self.settings = json.load(json_settings)
+        #         #add a way to store shapes in json file
+        #     # }
+        #     with open('default_settings.json', 'r') as default_settings:
+        #         self.settings = json.load(default_settings)
+        #     #loads the settings from the default file and saves them to a new settings file since there is none
+        #     with open('settings.json', 'w') as outfile:
+        #         json.dump(self.settings, outfile)
+        # else:
+        #     print('found settings file')
+        #     with open('settings.json', 'r') as json_settings:
+        #         self.settings = json.load(json_settings)
 
         # Force the style to be the same on all OSs:
         app.setStyle("Fusion")
@@ -113,41 +164,53 @@ class Maze_Controller(QWidget):
         #main_layout.addWidget(self.instances_table,0,1,2,0)
         
         layout1 = QVBoxLayout()
-        # layout1.addWidget(self.instances_table)
+        layout1.addWidget(self.instances_table)
         layout1.addWidget(self.start_button);layout1.addWidget(self.stop_button);layout1.addWidget(self.preview_button)
         main_layout.addLayout(layout1, 0,3,6,1)
 
 
         self.setLayout(main_layout)
-        self.gui_thread = Gui_updater()
+        self.processor = processor()
+        self.preview_thread = QThread()
+        self.stream_thread = QThread()
+        self.processor.frm.connect(self.display_frame)
+        
         # self.main_processing()
 
 
 
     def main_processing(self):
-        #get Qthreadpool to keep gui up to date
-        
-        self.gui_thread.update_image.connect(self.gui_update)
-        self.gui_thread.start()
+        #get Qthreadpool to keep gui up to date       
+        # self.thread.started.connect(self.gui_update)
+        self.processor.moveToThread(self.stream_thread)
+        self.stream_thread.started.connect(self.processor.grab_stream)
+        self.stream_thread.start()
 
     def preview(self):
-        self.gui_thread.update_image.connect(self.gui_update)
-        self.gui_thread.start()
+        # self.thread.started.connect(self.gui_preview)
+        self.processor.moveToThread(self.preview_thread)
+        self.preview_thread.started.connect(self.processor.grab_single  )
+        self.preview_thread.start()
 
     def shutdown_routine(self):
-        print("Shutdown Routine Activated")
+
         #add all shutdown commands here
-        self.gui_thread.stop()
+        self.processor.vid.release()
+        self.processor.out.release()
+        self.stream_thread.exit();self.preview_thread.exit()
         self.close()
+
+        print("Shutdown Routine Activated")
         return
 
 
     @pyqtSlot(np.ndarray)
-    def gui_update(self, Qframe):
-        Qframe = cv2.cvtColor(Qframe, cv2.COLOR_BGR2RGB)
+    def display_frame(self, frame):
+        Qframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         Qframe = QImage(Qframe, Qframe.shape[1], Qframe.shape[0], Qframe.strides[0],QImage.Format.Format_RGB888)
         pixmap = QPixmap.fromImage(Qframe)
         self.display_camera.setPixmap(pixmap)
+
     
     def calculation(self, dlc):
         time.sleep(1)
--- gui_video_example_ohmedit.py
+++ gui_video_example_ohmedit.py
@@ -1,7 +1,7 @@
-from dlclive import DLCLive, Processor
+# from dlclive import DLCLive, Processor
 import numpy as np
 import serial, cv2, sys, os, json, pickle
-from pypylon import pylon, genicam
+# from pypylon import pylon, genicam
 import time
 from datetime import datetime, date 
 
@@ -12,6 +12,53 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from qt_material import apply_stylesheet
+
+class processor(QObject):
+    frm = pyqtSignal(np.ndarray)
+
+    nnect
+    def __init__(self):
+        super().__init__()
+        self.settings = {}
+        self._run_flag = False
+        path = "C:\\Users\\ohmkp\\OneDrive\\Desktop\\vids\\"
+        self.dt = datetime.strftime(datetime.now(), "d%y.%m.%d_t%H.%M")
+        fourcc = cv2.VideoWriter_fourcc(*'XVID');fps = 30.0;frameSize = (1290, 720)
+        vid_path = path +  self.dt + "_recording.avi"
+        self.out = cv2.VideoWriter(vid_path, fourcc, fps, frameSize)
+        self.vid = pylon.InstantCamera();self.setup(self.settings)
+
+    def grab_single(self):
+        if not self.vid.isOpened():
+            self.vid.open(0)
+        ret, frame = self.vid.read()
+        if ret:
+            self.frm.emit(frame)
+        else:
+            sys.exit('the camera failed to capture a frame')
+        self.vid.release()
+        
+    def grab_stream(self):
+        self._run_flag = True
+        if not self.vid.isOpened():
+            self.vid.open(0)
+        while self._run_flag:
+            ret, frame = self.vid.read()
+            if ret:
+                self.frm.emit(frame)
+                self.out.write(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                sys.exit('the camera failed to capture a frame')
+        self.vid.release()
+        self.out.release()
+
+    def change_settings(self, new_settings):
+        self.settings = new_settings;   exp = self.settings['experiment'];cam = self.settings['camera']
+
+    def setup(self, settings):
+        self.settings = settings
+        cam = self.settings['camera'];exp = self.settings['experiment']
+
 
 
 class Gui_updater(QThread):
@@ -30,13 +77,7 @@ class Gui_updater(QThread):
         grab = self.vid.RetrieveResult(1000, pylon.TimeoutHandling_ThrowException)
         if grab.GrabSucceeded():
             frame = grab.GetArray()
-<<<<<<< HEAD
-            self.process_frame(frame)
-=======
-            QFrame = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format.Format_Grayscale8)
-            pixmap = QPixmap.fromImage(QFrame)
             self.update_image.emit(frame)
->>>>>>> 245b15613be206300696419e7ca67f9c5c88750a
         self.vid.Close()
 
     def run(self):
@@ -153,6 +194,8 @@ class QGB(QGridLayout):
         self.vspacer = QWidget();self.vspacer.setFixedHeight(25)
 
         self.gboxes = [self.gbox(i) for i in range(4)]
+        self.ser = serial.Serial();self.ser.port= 'COM5';self.ser.baudrate = 115200
+#######################################################################################################
 
 
     def vs_layout(self):
@@ -209,58 +252,61 @@ class QGB(QGridLayout):
         ##insert other func here
         return box
 
-class Maze_Controller(QWidget):
+class Maze_Controller(QWidget,QObject):
     def __init__(self):
         super().__init__()
-        if not os.path.exists('./settings.json'):
-            print('Missing settings file ... Loading defaults')
-            with open('default_settings.json', 'r') as default_settings:
-                self.settings = json.load(default_settings)
-            #loads the settings from the default file and saves them to a new settings file since there is none
-            with open('settings.json', 'w') as outfile:
-                json.dump(self.settings, outfile)
-        else:
-            print('Found settings file')
-            with open('settings.json', 'r') as json_settings:
-                self.settings = json.load(json_settings)
+        def load_settings():
+            if not os.path.exists('./settings.json'):
+                print('Missing settings file ... Loading defaults')
+                with open('default_settings.json', 'r') as default_settings:
+                    self.settings = json.load(default_settings)
+                #loads the settings from the default file and saves them to a new settings file since there is none
+                with open('settings.json', 'w') as outfile:
+                    json.dump(self.settings, outfile)
+            else:
+                print('Found settings file')
+                with open('settings.json', 'r') as json_settings:
+                    self.settings = json.load(json_settings)
+        load_settings()
         # self.vid = pylon.InstantCamera();self.setup()
         # self.conv = pylon.ImageFormatConverter()
 
-        # self.windowTitle('MazeController')
-        self.showFullScreen()
-        self.exit= QAction("Exit Application",shortcut=QKeySequence("Esc"),triggered=self.shutdown_routine)
-        self.addAction(self.exit)
-        apply_stylesheet(app, theme='dark_cyan.xml')
-
+        def win_setup():
+            self.windowTitle('MazeController')
+            self.showFullScreen()
+            self.exit= QAction("Exit Application",shortcut=QKeySequence("Esc"),triggered=self.shutdown_routine)
+            self.addAction(self.exit)
+            apply_stylesheet(app, theme='dark_cyan.xml')
+        win_setup()
         
-        self.tabs = QTabWidget()
-
-        self.tab_names = ['livestream', 'buttons']
-
-        livestream_layout = QVBoxLayout()
-        livestream_widget = QWidget()
-        livestream_lbl =  QLabel()
-        livestream_lbl.setFixedWidth(self.settings['camera']['width']);livestream_lbl.setFixedHeight(self.settings['camera']['height'])
-        self.data_table = QTableWidget();self.data_table.setRowCount(3);self.data_table.setColumnCount(3);self.data_table.setHorizontalHeaderLabels(['X', 'Y', 'prob.']);self.data_table.setVerticalHeaderLabels(['nose', 'center', 'tail'])
-        self.data_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-        livestream_layout.addWidget(livestream_lbl, Qt.AlignmentFlag.AlignCenter);livestream_layout.addWidget(self.data_table, Qt.AlignmentFlag.AlignCenter)
-        livestream = QWidget();livestream.setLayout(livestream_layout)
-        
-        self.grid = QGB()
-        buttons =  QWidget();buttons.setLayout(self.grid)
+        def layout_setup():
+            self.tabs = QTabWidget()
+            self.tab_names = ['livestream', 'buttons']
+            livestream_layout = QVBoxLayout()
+            livestream_widget = QWidget()
+            self.livestream_lbl =  QLabel()
+            self.livestream_lbl.setFixedWidth(self.settings['camera']['width']);self.livestream_lbl.setFixedHeight(self.settings['camera']['height'])
+            self.data_table = QTableWidget();self.data_table.setRowCount(3);self.data_table.setColumnCount(3);self.data_table.setHorizontalHeaderLabels(['X', 'Y', 'prob.']);self.data_table.setVerticalHeaderLabels(['nose', 'center', 'tail'])
+            self.data_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+            livestream_layout.addWidget(self.livestream_lbl, Qt.AlignmentFlag.AlignCenter);livestream_layout.addWidget(self.data_table, Qt.AlignmentFlag.AlignCenter)
+            livestream = QWidget();livestream.setLayout(livestream_layout)
+            self.grid = QGB()
+            buttons =  QWidget();buttons.setLayout(self.grid)
+            self.tab_dict = {'livestream':self.livestream, 'buttons':self.buttons}
+            for i in self.tab_names:
+                self.tabs.addTab(self.tab_dict[i], i)
+            self.main_layout = QHBoxLayout();self.main_layout.addWidget(self.tabs)
+            self.setLayout(self.main_layout)
+        layout_setup()
         
         # self.tabs.addTab(self.livestream, 'livestream')
-        
-        self.tab_dict = {'livestream':livestream, 'buttons':buttons}
-
-        for i in self.tab_names:
-            self.tabs.addTab(self.tab_dict[i], i)
-            
-        self.main_layout = QHBoxLayout();self.main_layout.addWidget(self.tabs)
-        self.setLayout(self.main_layout)
 
         self.wdir = os.path.dirname(os.path.realpath(__file__))
         self.grid.change_filepath.clicked.connect(self.pathprompt)
+
+        self.processor = processor()
+        self.preview_thread = QThread()
+        self.stream_thread = QThread()
 
 
     def button_setup(self):
@@ -271,31 +317,6 @@ class Maze_Controller(QWidget):
         self.grid.stop.clicked.connect(self.shutdown_routine)
         self.grid.change_filepath.clicked.connect(self.pathprompt)
 
-    def main_processing(self):
-        #get Qthreadpool to keep gui up to date
-        self.gui_thread = Gui_updater()
-        self.gui_thread.update_image.connect(self.gui_update)
-        self.gui_thread.start()
-
-    def shutdown_routine(self):
-        print("Shutdown Routine Activated")
-        #add all shutdown commands here
-        self.gui_thread.stop()
-        self.close()
-        return
-    
-    @pyqtSlot(np.ndarray)
-    def gui_update(self, Qframe):
-        # Qframe = cv2.cvtColor(Qframe, cv2.COLOR_BGR2RGB)
-        Qframe = QImage(Qframe, Qframe.shape[1], Qframe.shape[0], Qframe.strides[0],QImage.Format.Format_Grayscale8)
-        pixmap = QPixmap.fromImage(Qframe)
-        self.display_camera.setPixmap(pixmap)
-
-    def gui_preview(self, Qframe):
-        Qframe = QImage(Qframe, Qframe.shape[1], Qframe.shape[0], Qframe.strides[0],QImage.Format.Format_Grayscale8)
-        temp_pix = QPixmap.fromImage(Qframe)
-        self.display_camera.setPixmap(temp_pix)
-
     def pathprompt(self):
         options = QFileDialog.Option.ShowDirsOnly
         dlg = QFileDialog();dlg.setOptions(options);dlg.setFileMode(QFileDialog.FileMode.Directory)
@@ -303,10 +324,71 @@ class Maze_Controller(QWidget):
         self.wdir = fname
         msg = f'the working directory is: \"{str(fname)}\"'
         self.grid.path_label.setText(msg)
-    
-    def calculation(self, dlc):
-        time.sleep(1)
+
+    def main_processing(self):
+        #get Qthreadpool to keep gui up to date       
+        # self.thread.started.connect(self.gui_update)
+        self.processor.moveToThread(self.stream_thread)
+        self.stream_thread.started.connect(self.processor.grab_stream)
+        self.stream_thread.start()
+
+    def preview(self):
+        # self.thread.started.connect(self.gui_preview)
+        self.processor.moveToThread(self.preview_thread)
+        self.preview_thread.started.connect(self.processor.grab_single  )
+        self.preview_thread.start()
+
+    def shutdown_routine(self):
+        #add all shutdown commands here
+        self.processor.vid.release()
+        self.processor.out.release()
+        self.stream_thread.exit();self.preview_thread.exit()
+        self.close()
+
+        print("Shutdown Routine Activated")
         return
+
+    @pyqtSlot(np.ndarray)
+    def display_frame(self, frame):
+        Qframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        Qframe = QImage(Qframe, Qframe.shape[1], Qframe.shape[0], Qframe.strides[0],QImage.Format.Format_RGB888)
+        pixmap = QPixmap.fromImage(Qframe)
+        self.livestream_lbl.setPixmap(pixmap)
+
+    def process(self, pose):
+        if(self.in_sector(pose[0,0], pose[0,1])
+            and self.in_sector(pose[1,0], pose[1,1])
+            and self.in_sector(pose[2,0], pose[2,1])):
+                #the subject is in the sector, so shock is delivered (turned on if off)
+                if not self.shock_on:
+                    self.ser.write(self.command(5))
+                    self.shock_on = True
+                    #notes the time that the shock started, for later use
+                    self.start_timer = time()
+                   
+        else:
+                #shock is not delivered since the subject is not in the sector (turned off if on)
+                if self.shock_on:
+                    self.ser.write(self.command(6))
+                    self.shock_on = False
+                    #notes the time that the shock ended, subtracts from start and adds to the array
+                    self.final_time = time() - self.start_timer
+                    self.times_inzone = np.append(self.times_inzone, self.final_time)
+                   
+        return pose        
+
+    def in_sector(self, x, y):
+        # time.sleep(0)
+        vec_1 = np.array([x, y])
+        vec_2 = self.mag/2
+        diff = vec_1 - vec_2;ratio = diff[1]/diff[0]
+        theta = np.degrees(np.arctan(ratio))
+        # true if calculated theta is in the 60 degree sector from -30 to 30 degrees
+        if (theta < 30 and theta > -30):
+            #DELETE THIS LATER, just to make sure the math is correct for now
+            print("subject is in the shock zone!")
+            #boolean output for the shock function
+            return True
 
 
 
